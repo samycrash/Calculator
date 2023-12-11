a9b9ad7bf8f942bc12f71d6ea933e0543b9dba2b
samycrash <samuelcrash7@gmail.com>
2023-12-12 03:57:38 +0700

    Run and display The Front End app

--- calculator.py
+++ calculator.py
@@ -2,9 +2,10 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
-Builder.load_file('frontend.kv')
 
-class calculatorScreen(Screen):
+Builder.load_file("frontend.kv")
+
+class DisplayScreen(Screen):
     
     def multiply(self):
         pass
