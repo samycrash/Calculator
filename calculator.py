24705940443fb927de9a5f808a1704a89fff0c58
samycrash <samuelcrash7@gmail.com>
2023-12-12 02:44:52 +0700

    try empty calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,17 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+
+class calculatorScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class myCalculator(App):
+
+    def build(self):
+        return RootWidget()
+    
+
+myCalculator().run()
